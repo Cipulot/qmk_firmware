f735576f13587e5653fb95d88b2b3020cfed6eb2
Cipulot <40441626+Cipulot@users.noreply.github.com>
2021-03-06 13:53:14 +0100

    Change of copyright head

--- keyboards/handwired/oled_macro/oled_macro.c
+++ keyboards/handwired/oled_macro/oled_macro.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 Daniel Reibl <janos.daniel.reibl@protonmail.com>
+/* Copyright 2021 Cipulot
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
