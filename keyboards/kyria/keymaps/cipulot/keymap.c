c3551c4f782e8a2b7f5e2a428b88131e6f8a3907
Cipulot <40441626+Cipulot@users.noreply.github.com>
2021-06-27 16:24:51 +0200

    Formatting

--- keyboards/kyria/keymaps/cipulot/keymap.c
+++ keyboards/kyria/keymaps/cipulot/keymap.c
@@ -194,29 +194,28 @@ static void render_status(void) {
     oled_write_P(led_state.scroll_lock ? PSTR("SCRL") : PSTR("       "), false);
 }
 
-
 // WPM-responsive animation stuff here
-#define IDLE_FRAMES 5
-#define IDLE_SPEED 40 // below this wpm value your animation will idle
+#    define IDLE_FRAMES 5
+#    define IDLE_SPEED 40  // below this wpm value your animation will idle
 
 // #define PREP_FRAMES 1 // uncomment if >1
 
-#define TAP_FRAMES 2
-#define TAP_SPEED 50 // above this wpm value typing animation to triggere
+#    define TAP_FRAMES 2
+#    define TAP_SPEED 50  // above this wpm value typing animation to triggere
 
-#define ANIM_FRAME_DURATION 200 // how long each frame lasts in ms
+#    define ANIM_FRAME_DURATION 200  // how long each frame lasts in ms
 // #define SLEEP_TIMER 60000 // should sleep after this period of 0 wpm, needs fixing
-#define ANIM_SIZE 636 // number of bytes in array, minimize for adequate firmware size, max is 1024
+#    define ANIM_SIZE 636  // number of bytes in array, minimize for adequate firmware size, max is 1024
 
-#define LINE_SIZE 128
+#    define LINE_SIZE 128
 
-uint32_t anim_timer = 0;
-uint32_t anim_sleep = 0;
-uint8_t current_idle_frame = 0;
+uint32_t anim_timer         = 0;
+uint32_t anim_sleep         = 0;
+uint8_t  current_idle_frame = 0;
 // uint8_t current_prep_frame = 0; // uncomment if PREP_FRAMES >1
 uint8_t current_tap_frame = 0;
 
-static const char PROGMEM base_frame[636]    = {0,  0,  126, 126, 24, 60, 102, 66,  0,   12,  28,  112, 112, 28, 12, 0,  116, 116, 20, 20, 124, 104, 0,  124, 124, 0, 112, 120, 44,  36, 124, 124, 0,   0, 0,   0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0,  0, 0, 0,   0,  0,  0,   0,   0,   0,   0,   128, 64,  64,  32,  32,  32,  32,  16,  16,  16,  16,  16,  8,   8,   4,   4, 4,   8,  48,  64, 128, 0, 0,   0, 0, 0,   0, 0, 0, 0, 0,  0,  0,  0,  0,   0,  0,  0,  0,  0,  0,  0,  0,  0,   0,  0,  0,  0,  0,  0,  0,  0,   0,   0,  0,  0,  0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 128, 128, 128, 0,  0,  0,  0,  192, 96, 48, 24, 12, 132, 198, 98, 35, 51, 17, 145, 113, 241, 113, 145, 17, 51, 35, 98, 198, 132, 12,  24,  48,  96,  192, 0,   0, 0,   0,   0,   0,   0,   0,   0,   0, 0, 0, 0, 0, 0, 0,  0,   0,   24, 100, 130, 2,   2,   2,   2,  2,  1,  0,  0,   0, 0,   128, 128, 0,  0,  0,  0,  0,   0,   0,   0,  0,  128, 0,   48,  48, 0,  192, 193, 193, 194, 4, 8,
+static const char PROGMEM base_frame[636] = {0,  0,  126, 126, 24, 60, 102, 66,  0,   12,  28,  112, 112, 28, 12, 0,  116, 116, 20, 20, 124, 104, 0,  124, 124, 0, 112, 120, 44,  36, 124, 124, 0,   0, 0,   0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0,  0, 0, 0,   0,  0,  0,   0,   0,   0,   0,   128, 64,  64,  32,  32,  32,  32,  16,  16,  16,  16,  16,  8,   8,   4,   4, 4,   8,  48,  64, 128, 0, 0,   0, 0, 0,   0, 0, 0, 0, 0,  0,  0,  0,  0,   0,  0,  0,  0,  0,  0,  0,  0,  0,   0,  0,  0,  0,  0,  0,  0,  0,   0,   0,  0,  0,  0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 128, 128, 128, 0,  0,  0,  0,  192, 96, 48, 24, 12, 132, 198, 98, 35, 51, 17, 145, 113, 241, 113, 145, 17, 51, 35, 98, 198, 132, 12,  24,  48,  96,  192, 0,   0, 0,   0,   0,   0,   0,   0,   0,   0, 0, 0, 0, 0, 0, 0,  0,   0,   24, 100, 130, 2,   2,   2,   2,  2,  1,  0,  0,   0, 0,   128, 128, 0,  0,  0,  0,  0,   0,   0,   0,  0,  128, 0,   48,  48, 0,  192, 193, 193, 194, 4, 8,
                                              16, 32, 64,  128, 0,  0,  0,   128, 128, 128, 128, 64,  64,  64, 64, 32, 32,  32,  32, 16, 16,  16,  16, 8,   8,   8, 8,   8,   196, 4,  196, 4,   196, 2, 194, 2, 194, 1, 1, 1, 1, 0, 0, 0, 0, 0, 252, 15, 1, 0, 248, 14, 31, 109, 140, 148, 148, 164, 166, 249, 224, 255, 224, 249, 166, 164, 148, 148, 140, 109, 31,  14,  248, 0, 1,   15, 252, 0,  0,   0, 0,   0, 0, 0,   0, 0, 0, 0, 0,  0,  0,  0,  192, 56, 4,  3,  0,  0,  0,  0,  0,  0,   0,  12, 12, 12, 13, 1,  0,  64,  160, 33, 34, 18, 17, 17, 17, 9, 8, 8, 8, 8, 4, 4, 8, 8,   16,  16,  16, 16, 16, 17, 15,  1,  1,  0,  0,  0,   0,   0,  0,  0,  0,  0,   0,   0,   0,   0,   0,  0,  0,  0,  0,   170, 170, 255, 255, 195, 191, 127, 3, 127, 191, 195, 255, 255, 170, 170, 0, 0, 0, 0, 0, 0, 31, 120, 192, 0,  15,  56,  124, 219, 152, 20, 20, 18, 50, 207, 3, 255, 3,   207, 50, 18, 20, 20, 152, 219, 124, 56, 15, 0,   192, 120, 31, 16, 16,  16,  16,  8,   8, 8,
                                              8,  8,  4,   4,   4,  4,  4,   2,   3,   2,   2,   1,   1,   1,  1,  1,  1,   2,   2,  4,  4,   8,   8,  8,   8,   8, 7,   0,   0,   0,  0,   0,   0,   0, 0,   0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0,  0, 0, 0,   0,  0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   2,   130, 135, 31,  7, 159, 7,  28,  7,  159, 7, 159, 7, 2, 130, 0, 0, 0, 0, 32, 16, 16, 16, 17,  11, 14, 12, 24, 16, 49, 35, 98, 102, 68, 68, 71, 71, 71, 68, 68, 102, 98,  35, 49, 16, 24, 12, 6,  3, 1, 0, 0, 0, 0, 0, 0, 0,   0,   0,   0,  0,  0,  0,  0,   0,  0,  0,  0,  0,   0,   0,  0,  0,  0,  0,   0,   0,   0,   0,   0,  0,  0,  0,  0,   0,   0,   0,   0,   0,   0,   0,   0, 0,   0,   0,   0,   0,   0,   0,   0, 0, 0, 0, 0, 0, 0,  0,   0,   0,  0,   0,   0,   0,   0,   0,  0,  0,  0,  0,   0, 0,   0,   0,   0,  0,  0,  0,  0,   7,   8,   8,  23, 0,   15,  1,   2,  1,  15,  0,   15,  2,   5, 8};
 
@@ -296,7 +295,6 @@ void write_frame_at_index(int index) {
             break;
     }
 }
-
 
 // Images credit j-inc(/James Incandenza) and pixelbenny. Credit to obosob for initial animation approach.
 static void render_anim(void) {
@@ -399,18 +397,18 @@ static void render_anim(void) {
     }
     */
 
-    if(get_current_wpm() != 0) {
-        oled_on(); // not essential but turns on animation OLED with any alpha keypress
-        if(timer_elapsed32(anim_timer) > ANIM_FRAME_DURATION) {
+    if (get_current_wpm() != 0) {
+        oled_on();  // not essential but turns on animation OLED with any alpha keypress
+        if (timer_elapsed32(anim_timer) > ANIM_FRAME_DURATION) {
             anim_timer = timer_read32();
             animation_phase();
         }
         anim_sleep = timer_read32();
     } else {
-        if(timer_elapsed32(anim_sleep) > OLED_TIMEOUT) {
+        if (timer_elapsed32(anim_sleep) > OLED_TIMEOUT) {
             oled_off();
         } else {
-            if(timer_elapsed32(anim_timer) > ANIM_FRAME_DURATION) {
+            if (timer_elapsed32(anim_timer) > ANIM_FRAME_DURATION) {
                 anim_timer = timer_read32();
                 animation_phase();
             }
@@ -420,22 +418,22 @@ static void render_anim(void) {
 
 void oled_task_user(void) {
     if (is_keyboard_master()) {
-        render_status(); // Renders the current keyboard state (layer, lock, caps, scroll, etc)
+        render_status();  // Renders the current keyboard state (layer, lock, caps, scroll, etc)
     } else {
-        //render_kyria_logo();
+        // render_kyria_logo();
         render_anim();
         uint8_t n = get_current_wpm();
-        char wpm_counter[6];
+        char    wpm_counter[6];
         wpm_counter[5] = '\0';
         wpm_counter[4] = ' ';
         wpm_counter[3] = ' ';
         wpm_counter[2] = '0' + n % 10;
-        wpm_counter[1] = ( n /= 10) % 10 ? '0' + (n) % 10 : (n / 10) % 10 ? '0' : ' ';
+        wpm_counter[1] = (n /= 10) % 10 ? '0' + (n) % 10 : (n / 10) % 10 ? '0' : ' ';
         wpm_counter[0] = n / 10 ? '0' + n / 10 : ' ';
-        oled_set_cursor(0,6);
+        oled_set_cursor(0, 6);
         oled_write("       WPM:", false);
-        if (wpm_counter[0]==' ') {
-            if (wpm_counter[1]==' ') {
+        if (wpm_counter[0] == ' ') {
+            if (wpm_counter[1] == ' ') {
                 oled_write(&wpm_counter[2], false);
             } else {
                 wpm_counter[4] = '\0';
@@ -458,8 +456,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         } else {
             tap_code(KC_VOLD);
         }
-    }
-    else if (index == 1) {
+    } else if (index == 1) {
         // Page up/Page down
         if (clockwise) {
             tap_code(KC_PGDN);
