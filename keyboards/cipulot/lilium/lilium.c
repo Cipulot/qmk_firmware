a96dffc9f5dcec37b6df46844c0784bed0c11e03
Cipulot <40441626+Cipulot@users.noreply.github.com>
2024-10-04 02:00:56 +0200

    Stuff

--- keyboards/cipulot/lilium/lilium.c
+++ keyboards/cipulot/lilium/lilium.c
@@ -28,8 +28,8 @@ void eeconfig_init_kb(void) {
     eeprom_ec_config.mode_0_actuation_threshold     = DEFAULT_MODE_0_ACTUATION_LEVEL;
     eeprom_ec_config.mode_0_release_threshold       = DEFAULT_MODE_0_RELEASE_LEVEL;
     eeprom_ec_config.mode_1_initial_deadzone_offset = DEFAULT_MODE_1_INITIAL_DEADZONE_OFFSET;
-    eeprom_ec_config.mode_1_actuation_offset   = DEFAULT_MODE_1_ACTUATION_OFFSET;
-    eeprom_ec_config.mode_1_release_offset     = DEFAULT_MODE_1_RELEASE_OFFSET;
+    eeprom_ec_config.mode_1_actuation_offset        = DEFAULT_MODE_1_ACTUATION_OFFSET;
+    eeprom_ec_config.mode_1_release_offset          = DEFAULT_MODE_1_RELEASE_OFFSET;
 
     for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
         for (uint8_t col = 0; col < MATRIX_COLS; col++) {
@@ -52,8 +52,8 @@ void keyboard_post_init_kb(void) {
     ec_config.mode_0_actuation_threshold     = eeprom_ec_config.mode_0_actuation_threshold;
     ec_config.mode_0_release_threshold       = eeprom_ec_config.mode_0_release_threshold;
     ec_config.mode_1_initial_deadzone_offset = eeprom_ec_config.mode_1_initial_deadzone_offset;
-    ec_config.mode_1_actuation_offset   = eeprom_ec_config.mode_1_actuation_offset;
-    ec_config.mode_1_release_offset     = eeprom_ec_config.mode_1_release_offset;
+    ec_config.mode_1_actuation_offset        = eeprom_ec_config.mode_1_actuation_offset;
+    ec_config.mode_1_release_offset          = eeprom_ec_config.mode_1_release_offset;
     ec_config.bottoming_calibration          = false;
     for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
         for (uint8_t col = 0; col < MATRIX_COLS; col++) {
@@ -62,6 +62,8 @@ void keyboard_post_init_kb(void) {
             ec_config.rescaled_mode_0_actuation_threshold[row][col]     = rescale(ec_config.mode_0_actuation_threshold, 0, 1023, ec_config.noise_floor[row][col], eeprom_ec_config.bottoming_reading[row][col]);
             ec_config.rescaled_mode_0_release_threshold[row][col]       = rescale(ec_config.mode_0_release_threshold, 0, 1023, ec_config.noise_floor[row][col], eeprom_ec_config.bottoming_reading[row][col]);
             ec_config.rescaled_mode_1_initial_deadzone_offset[row][col] = rescale(ec_config.mode_1_initial_deadzone_offset, 0, 1023, ec_config.noise_floor[row][col], eeprom_ec_config.bottoming_reading[row][col]);
+            ec_config.rescaled_mode_1_actuation_offset[row][col]        = rescale(ec_config.mode_1_actuation_offset, 0, 1023, ec_config.noise_floor[row][col], eeprom_ec_config.bottoming_reading[row][col]);
+            ec_config.rescaled_mode_1_release_offset[row][col]          = rescale(ec_config.mode_1_release_offset, 0, 1023, ec_config.noise_floor[row][col], eeprom_ec_config.bottoming_reading[row][col]);
         }
     }
 
