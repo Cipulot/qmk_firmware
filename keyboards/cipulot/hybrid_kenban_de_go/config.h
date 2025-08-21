9bdf838fadeac93573e1dc402c30525e8e00ee5c
Cipulot <40441626+Cipulot@users.noreply.github.com>
2025-08-21 20:00:09 +0200

    WIP

---
+++ keyboards/cipulot/hybrid_kenban_de_go/config.h
@@ -0,0 +1,50 @@
+/* Copyright 2025 Cipulot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 7
+
+#define DEFAULT_ACTUATION_MODE 0
+#define DEFAULT_MODE_0_ACTUATION_LEVEL 550
+#define DEFAULT_MODE_0_RELEASE_LEVEL 500
+#define DEFAULT_MODE_1_INITIAL_DEADZONE_OFFSET DEFAULT_MODE_0_ACTUATION_LEVEL
+#define DEFAULT_MODE_1_ACTUATION_OFFSET 70
+#define DEFAULT_MODE_1_RELEASE_OFFSET 70
+#define DEFAULT_EXTREMUM 1023
+#define EXPECTED_NOISE_FLOOR 0
+#define NOISE_FLOOR_THRESHOLD 50
+#define BOTTOMING_CALIBRATION_THRESHOLD 100
+#define DEFAULT_NOISE_FLOOR_SAMPLING_COUNT 30
+#define DEFAULT_BOTTOMING_READING 1023
+#define DEFAULT_CALIBRATION_STARTER true
+
+#define DISCHARGE_TIME 10
+
+#define EECONFIG_KB_DATA_SIZE 142
+
+// Split keyboard configuration
+#define SERIAL_USART_DRIVER SD1
+#define SERIAL_USART_TX_PIN A9 // USART1 TX pin
+
+// OLED configuration
+#ifdef OLED_ENABLE
+#    define SPLIT_OLED_ENABLE
+#    define OLED_DISPLAY_128X64
+#endif
+
+#define SPLIT_TRANSACTION_IDS_KB RPC_ID_VIA_CMD
