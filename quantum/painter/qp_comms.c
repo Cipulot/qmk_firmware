c355e433feed5b6eb1d865e449f930222c6fd2da
Cipulot <40441626+Cipulot@users.noreply.github.com>
2025-08-30 17:34:22 +0200

    WIP QP

--- quantum/painter/qp_comms.c
+++ quantum/painter/qp_comms.c
@@ -52,7 +52,7 @@ uint32_t qp_comms_send(painter_device_t device, const void *data, uint32_t byte_
 bool qp_comms_command(painter_device_t device, uint8_t cmd) {
     painter_driver_t *                   driver       = (painter_driver_t *)device;
     painter_comms_with_command_vtable_t *comms_vtable = (painter_comms_with_command_vtable_t *)driver->comms_vtable;
-    comms_vtable->send_command(device, cmd);
+    return comms_vtable->send_command(device, cmd);
 }
 
 bool qp_comms_command_databyte(painter_device_t device, uint8_t cmd, uint8_t data) {
