6b0ad9b5349bb1cded1dd7b97a812d7ff133d145
Cipulot <40441626+Cipulot@users.noreply.github.com>
2026-01-07 05:00:02 +0100

    WIP

---
+++ keyboards/cipulot/a/ec_board.c
@@ -0,0 +1,95 @@
+/* Copyright 2026 Cipulot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ec_switch_matrix.h"
+#include "keyboard.h"
+
+#ifdef SPLIT_KEYBOARD
+#    include "transactions.h"
+#endif
+
+// Definition of SOCD shared instance
+socd_cleaner_t socd_opposing_pairs[4];
+
+void eeconfig_init_kb(void) {
+    // Initialize all keys with default values
+    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+        for (uint8_t col = 0; col < MATRIX_COLS; col++) {
+            key_state_t *key                = &eeprom_ec_config.key_state[row][col];
+            key->actuation_mode             = DEFAULT_ACTUATION_MODE;
+            key->apc_actuation_threshold    = DEFAULT_APC_ACTUATION_LEVEL;
+            key->apc_release_threshold      = DEFAULT_APC_RELEASE_LEVEL;
+            key->rt_initial_deadzone_offset = DEFAULT_RT_INITIAL_DEADZONE_OFFSET;
+            key->rt_actuation_offset        = DEFAULT_RT_ACTUATION_OFFSET;
+            key->rt_release_offset          = DEFAULT_RT_RELEASE_OFFSET;
+            key->bottoming_reading          = DEFAULT_BOTTOMING_READING;
+        }
+    }
+
+    // Initialize the SOCD cleaner pairs
+    const struct {
+        uint16_t key1;
+        uint16_t key2;
+    } socd_pairs[] = {
+        {KC_A, KC_D},
+        {KC_W, KC_S},
+        {KC_Z, KC_X},
+        {KC_LEFT, KC_RIGHT},
+    };
+
+    for (int i = 0; i < 4; i++) {
+        eeprom_ec_config.socd_opposing_pairs[i].keys[0]    = socd_pairs[i].key1;
+        eeprom_ec_config.socd_opposing_pairs[i].keys[1]    = socd_pairs[i].key2;
+        eeprom_ec_config.socd_opposing_pairs[i].resolution = SOCD_CLEANER_OFF;
+        eeprom_ec_config.socd_opposing_pairs[i].held[0]    = false;
+        eeprom_ec_config.socd_opposing_pairs[i].held[1]    = false;
+    }
+
+    // Write default value to EEPROM now
+    eeconfig_update_kb_datablock(&eeprom_ec_config, 0, EECONFIG_KB_DATA_SIZE);
+
+    eeconfig_init_user();
+}
+
+// On Keyboard startup
+void keyboard_post_init_kb(void) {
+    // Read custom menu variables from memory
+    eeconfig_read_kb_datablock(&eeprom_ec_config, 0, EECONFIG_KB_DATA_SIZE);
+
+    ec_config.bottoming_calibration = false;
+
+    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+        for (uint8_t col = 0; col < MATRIX_COLS; col++) {
+            key_state_t *key_eeprom  = &eeprom_ec_config.key_state[row][col];
+            key_state_t *key_runtime = &ec_config.key_state[row][col];
+
+            // Copy from EEPROM to runtime
+            *key_runtime = *key_eeprom;
+
+            // Initialize runtime-only values (noise_floor already set by ec_noise_floor())
+            key_runtime->bottoming_calibration_starter = true;
+            rescale_key_thresholds(key_runtime);
+        }
+    }
+
+#ifdef SPLIT_KEYBOARD
+    transaction_register_rpc(RPC_ID_VIA_CMD, via_cmd_slave_handler);
+#endif
+
+    memcpy(socd_opposing_pairs, eeprom_ec_config.socd_opposing_pairs, sizeof(socd_opposing_pairs));
+
+    keyboard_post_init_user();
+}
---
+++ keyboards/cipulot/a/ec_switch_matrix.h
@@ -0,0 +1,91 @@
+/* Copyright 2026 Cipulot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "matrix.h"
+#include "eeconfig.h"
+#include "util.h"
+#include "socd_cleaner.h"
+
+// Per-key state structure for hybrid switches (EC/MX)
+typedef struct PACKED {
+    uint8_t  actuation_mode;          // 0: APC, 1: Rapid Trigger
+    uint16_t apc_actuation_threshold; // per-key thresholds for mode 0
+    uint16_t apc_release_threshold;
+    uint16_t rt_initial_deadzone_offset; // per-key thresholds for mode 1
+    uint8_t  rt_actuation_offset;
+    uint8_t  rt_release_offset;
+
+    uint16_t noise_floor;                   // detected noise floor
+    uint16_t extremum;                      // rapid-trigger extremum tracker
+    bool     bottoming_calibration_starter; // pending bottoming sample flag
+    uint16_t bottoming_reading;             // per-key bottoming reading
+
+    uint16_t rescaled_apc_actuation_threshold; // rescaled thresholds using noise_floor/bottoming
+    uint16_t rescaled_apc_release_threshold;
+    uint16_t rescaled_rt_initial_deadzone_offset;
+    uint8_t  rescaled_rt_actuation_offset;
+    uint8_t  rescaled_rt_release_offset;
+} key_state_t;
+
+typedef struct PACKED {
+    // Per-key persistent data (includes thresholds, calibration and actuation_mode)
+    key_state_t    key_state[MATRIX_ROWS][MATRIX_COLS];
+    socd_cleaner_t socd_opposing_pairs[4]; // SOCD
+} eeprom_ec_config_t;
+
+typedef struct {
+    bool        bottoming_calibration;
+    key_state_t key_state[MATRIX_ROWS][MATRIX_COLS];
+} ec_config_t;
+
+// Check if the size of the reserved persistent memory is the same as the size of struct eeprom_ec_config_t
+_Static_assert(sizeof(eeprom_ec_config_t) == EECONFIG_KB_DATA_SIZE, "Mismatch in keyboard EECONFIG stored data");
+
+extern eeprom_ec_config_t eeprom_ec_config;
+
+extern ec_config_t ec_config;
+
+extern socd_cleaner_t socd_opposing_pairs[4];
+
+void init_row(void);
+void init_amux(void);
+void disable_unused_row(uint8_t row);
+void select_amux_channel(uint8_t channel, uint8_t col);
+void disable_unused_amux(uint8_t channel);
+void discharge_capacitor(void);
+void charge_capacitor(uint8_t row);
+
+int      ec_init(void);
+void     ec_noise_floor(void);
+bool     ec_matrix_scan(matrix_row_t current_matrix[]);
+uint16_t ec_readkey_raw(uint8_t channel, uint8_t row, uint8_t col);
+bool     ec_update_key(matrix_row_t *current_row, uint8_t row, uint8_t col, uint16_t sw_value);
+void     ec_print_matrix(void);
+void     rescale_key_thresholds(key_state_t *key);
+
+uint16_t rescale(uint16_t x, uint16_t out_min, uint16_t out_max);
+
+#ifdef UNUSED_POSITIONS_LIST
+bool is_unused_position(uint8_t row, uint8_t col);
+#endif
+
+#ifdef SPLIT_KEYBOARD
+void via_cmd_slave_handler(uint8_t m2s_size, const void *m2s_buffer, uint8_t s2m_size, void *s2m_buffer);
+#endif
